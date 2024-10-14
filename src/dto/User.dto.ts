8a26314d0346a74f37dce2ee92204dcc39b656fc
elquna <nsimamfon@gmail.com>
2024-10-14 09:46:14 +0100

    deploy

--- src/dto/User.dto.ts
+++
@@ -1,7 +0,0 @@
-export class UserPayload {
-    first_name: string;
-    email: string;
-    last_name: string;
-    phone:string;
-    age:number
-  }
--- test/userModel.ts
+++
@@ -1,8 +0,0 @@
-export interface User {
-  id: number;
-  name: string;
-  email: string;
-  phone: string
-}
-
-export const users: User[] = [];
